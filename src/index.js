caf202642c5752cf2cc4624f43e2499ed825bb79
Anna Ignateva <gannaioannovna25@gmail.com>
2024-02-29 22:31:01 +0400

    theme and filter by search

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux'
-mport { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 
-import {store} from './store'
+import { store } from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-  <Provider store={store}>
-  <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-    
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
